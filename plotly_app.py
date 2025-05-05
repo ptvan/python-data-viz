c977d3be040c2175fc8765743019e6ee5352c3eb
ptvan <nulzilla@gmail.com>
2025-05-05 06:00:29 -0700

    minimal plotly app

--- plotly_app.py
+++ plotly_app.py
@@ -1,3 +1,23 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+
+df = pd.read_csv('~/working/datasets/house_prices/Seattle_house_listings.csv')
+app = Dash()
+
+app.layout = [
+    html.H1(children='Seattle house prices', style={'textAlign':'center'}),
+    dcc.Dropdown(df.neighborhood.unique(), 'Parkwood', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+]
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.neighborhood==value]
+    return px.line(dff, x='house_sqft', y='listed_price')
+
+if __name__ == '__main__':
+    app.run(debug=True)
