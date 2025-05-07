2f19b95fbbf49faad699c8ce8087b233d7130bd8
ptvan <nulzilla@gmail.com>
2025-05-07 05:41:32 -0700

    updates

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,19 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-mtcars = pd.read_csv("~/working/datasets/R_defaults/mtcars.csv") 
+DATA_URL = '~/working/datasets/house_prices/Seattle_house_listings.csv'
+
+@st.cache_data
+def load_data(file):
+    data = pd.read_csv(DATA_URL)
+    return data
+
+st.title('Seattle house prices')
+
+# data_load_state = st.text('Loading data...')
+data = load_data(DATA_URL)
+# data_load_state.text("Done!")
+
+st.write(data)
+st.bar_chart(data, x="neighborhood", y="listed_price", color="n_bedrooms", stack=False)
