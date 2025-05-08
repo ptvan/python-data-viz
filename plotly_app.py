aa68561f2eb26d1d6cc123a85022207c1b8b943b
ptvan <nulzilla@gmail.com>
2025-05-08 05:36:37 -0700

    updates

--- plotly_app.py
+++ plotly_app.py
@@ -6,7 +6,7 @@ df = pd.read_csv('~/working/datasets/house_prices/Seattle_house_listings.csv')
 app = Dash()
 
 app.layout = [
-    html.H1(children='Seattle house prices', style={'textAlign':'center'}),
+    html.H1(children='Seattle house prices', style = {'textAlign':'center'}),
     dcc.Dropdown(df.neighborhood.unique(), 'Parkwood', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ]
@@ -16,8 +16,8 @@ app.layout = [
     Input('dropdown-selection', 'value')
 )
 def update_graph(value):
-    dff = df[df.neighborhood==value]
-    return px.line(dff, x='house_sqft', y='listed_price')
+    dff = df[df.neighborhood == value]
+    return px.line(dff, x = 'house_sqft', y = 'listed_price')
 
 if __name__ == '__main__':
     app.run(debug=True)
